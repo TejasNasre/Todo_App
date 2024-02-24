d8de6fc31a3f2cb9d25abea3beb1e8f0f3030ade
tejasnasre <tejasnasre120@gmail.com>
2024-02-24 20:39:03 +0530

    Complete Auth Used bcryptjs to auth and used hash to secure password

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -5,11 +5,12 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique : true,
     },
     email: {
       type: String,
       required: true,
-      unique : true,
+      unique: true,
     },
     password: {
       type: String,
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,17 +1,39 @@
 const router = require("express").Router();
 const User = require("../models/user.model.js");
+const bcrypt = require("bcryptjs");
 
 //SignIn Router
 router.post("/register", async (req, res) => {
     try {
         const {username, email, password} = req.body;
-        const user = new User({username,email,password});
+        const hashPassword = bcrypt.hashSync(password);
+        const user = new User({username,email,password : hashPassword});
         await user.save().then(() => 
             res.status(200).json({user : user}))
     } catch (error) {
         const err = res.status(400).json({messgae : 'User Already Exist !'})
         console.log(err)
     }
+});
+
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({email : req.body.email});
+        // console.log(user);
+        if(!user){
+            res.status(400).json({message : 'Please Register First'})
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
+        if(!isPasswordCorrect){
+            res.status(400).json({message : 'Invalid Password'})
+        }
+        const {password, ...others} = user._doc;
+        res.status(200).json({others})
+    }
+    catch(error){
+        res.status(400).json({message : 'Invalid User'})
+}
 });
 
 module.exports = router;
